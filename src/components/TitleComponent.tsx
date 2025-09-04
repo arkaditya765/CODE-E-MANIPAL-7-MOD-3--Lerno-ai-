99a4566406e47a64181dc17e93e35f9f69abd21a
arkaditya765 <arkaditya765@gmail.com>
2025-09-04 19:48:40 +0530

    Update TitleComponent.tsx

--- src/components/TitleComponent.tsx
+++ src/components/TitleComponent.tsx
@@ -1,14 +1,18 @@
-
 import { AnimatedShinyText } from "@/ui/animated-shiny-text";
 import { cn } from "@/lib/utils";
-const TitleComponent = ({ title }) => {
+
+interface TitleComponentProps {
+  title: string; // explicitly type the prop
+}
+
+const TitleComponent: React.FC<TitleComponentProps> = ({ title }) => {
   return (
     <div
       className={cn(
-        "group rounded-full border  text-base text-white transition-all ease-in hover:cursor-pointer  border-white/5 bg-neutral-900 hover:bg-neutral-800"
+        "group rounded-full border text-base text-white transition-all ease-in hover:cursor-pointer border-white/5 bg-neutral-900 hover:bg-neutral-800"
       )}
     >
-      <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out  hover:duration-300 hover:text-neutral-400">
+      <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:duration-300 hover:text-neutral-400">
         <span>{title}</span>
       </AnimatedShinyText>
     </div>
@@ -16,3 +20,4 @@ const TitleComponent = ({ title }) => {
 };
 
 export default TitleComponent;
+
